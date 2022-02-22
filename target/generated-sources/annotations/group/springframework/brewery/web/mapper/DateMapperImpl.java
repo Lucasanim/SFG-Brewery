bcc220380ca4c6e58f4ec4385620e83b684e2419
lucas <lucasanim12@gmail.com>
2022-02-21 21:01:44 -0300

    Fixed test

---
+++ target/generated-sources/annotations/group/springframework/brewery/web/mapper/DateMapperImpl.java
@@ -0,0 +1,14 @@
+package group.springframework.brewery.web.mapper;
+
+import org.springframework.stereotype.Component;
+
+/*
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-02-21T21:01:19-0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
+)
+*/
+@Component
+public class DateMapperImpl extends DateMapper {
+}
